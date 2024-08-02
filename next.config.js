5469b2fa3d6f61bd1bc9530ae78754200a2a0b9e
Rasil07 <rasilgrt@gmail.com>
2024-08-02 21:14:26 +1000

    removed env from next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
-  },
+  // env: {
+  //   DATABASE_URL: process.env.DATABASE_URL,
+  //   POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+  // },
 };
 
 module.exports = nextConfig;
